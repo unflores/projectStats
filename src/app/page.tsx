e7a76f8983c7091a029cafe3d3e9cd498f33d5aa
Austin Flores <ctrl4ltdeleteme@gmail.com>
2024-11-25 17:16:00 +0100

    Add graph for release candidates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
+"use client";
+import Card from "@/rcl/atoms/Card";
+import Title from "@/rcl/atoms/Title";
+import LineGraph from "@/rcl/graphs/LineGraph";
 
+
+
+const lines = [
+  {
+    id: "Monolith",
+    color: "hsl(220,78%,50%)",
+    data: [
+      { x: '2024-01', y: 1 },
+      { x: '2024-02', y: 20 },
+      { x: '2024-03', y: 5 },
+      { x: '2024-04', y: 8 },
+      { x: '2024-05', y: 11 },
+    ],
+  },
+];
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between min-h-screen p-5 items-left">
-
-      <div>Hai</div>
+    <div className="flex flex-col min-h-screen p-5">
+      <Card>
+        <Title level={1}>RC Frequency</Title>
+        <LineGraph className="h-[400px]" lines={lines} xLabel="Date" yLabel="Release Candidates" />
+      </Card>
     </div>
   );
 }
