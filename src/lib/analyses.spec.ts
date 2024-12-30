6924bb8bb75f6c97a2072025fb63e276f104ef46
Austin Flores <ctrl4ltdeleteme@gmail.com>
2024-12-30 23:28:21 +0100

    Fix jest config and add a test

---
+++ src/lib/analyses.spec.ts
@@ -0,0 +1,36 @@
+import { toCoalescedCounts } from "./analyses";
+
+describe("toCoalescedCounts", () => {
+  it("adds counts of previous day to the next day in same month", () => {
+    const counts = [
+      { date: new Date("2024-01-01"), count: 41 },
+      { date: new Date("2024-01-02"), count: 1 },
+    ];
+    const coalescedCounts = toCoalescedCounts(counts);
+
+    expect(coalescedCounts[0].count).toEqual(41);
+    expect(coalescedCounts[1].count).toEqual(42);
+  });
+
+  it("does NOT add count of previous day to the next day in new month", () => {
+    const counts = [
+      { date: new Date("2024-01-01"), count: 41 },
+      { date: new Date("2024-02-01"), count: 1 },
+    ];
+    const coalescedCounts = toCoalescedCounts(counts);
+
+    expect(coalescedCounts[0].count).toEqual(41);
+    expect(coalescedCounts[1].count).toEqual(1);
+  });
+
+  it("resets count of first of month when a month is skipped", () => {
+    const counts = [
+      { date: new Date("2024-01-01"), count: 41 },
+      { date: new Date("2024-03-10"), count: 1 },
+    ];
+    const coalescedCounts = toCoalescedCounts(counts);
+    expect(coalescedCounts[0].count).toEqual(41);
+    expect(coalescedCounts[1].count).toEqual(0);
+    expect(coalescedCounts[2].count).toEqual(1);
+  });
+});
